e7b7135945adaa09fe89c18156340310838f1046
Tim Herker <113173499+ti-he@users.noreply.github.com>
2024-04-27 14:12:11 +0200

    CHG: add propety for extension config and remove group name from target name

--- libs/vscode/nx-project-view/src/lib/views/nx-project-base-view.ts
+++ libs/vscode/nx-project-view/src/lib/views/nx-project-base-view.ts
@@ -10,9 +10,12 @@ import { TreeItemCollapsibleState } from 'vscode';
 declare module 'nx/src/devkit-exports' {
   interface TargetConfiguration {
     // Nx targets can have arbitrary additional properties.
-    // This allows users to assign a group to a target.
-    group?: string;
+    nxConsole?: NxConsoleConfig;
   }
+}
+
+interface NxConsoleConfig {
+  group?: string;
 }
 
 export interface ProjectViewStrategy<T> {
@@ -107,7 +110,7 @@ export abstract class BaseView {
       this.createTargetTreeItem(nxProject, target)
     );
     const treeItems = this.groupTargets(nxProject, targetViewItems);
-    // sort alphabetically
+    // Sort alphabetically, but make sure
     // groups come before ungrouped targets
     treeItems.sort((a, b) => {
       if (a.contextValue === 'group' && b.contextValue !== 'group') {
@@ -118,7 +121,7 @@ export abstract class BaseView {
       }
       return a.label.localeCompare(b.label);
     });
-    // also sort each group
+    // Also sort each group
     treeItems.forEach((item) => {
       if (item.contextValue === 'group') {
         item.targetViewItems.sort((a, b) => a.label.localeCompare(b.label));
@@ -162,7 +165,6 @@ export abstract class BaseView {
       result.push(group);
     }
 
-    // Add any targets that don't have a group
     const ungroupedTargets = targets.filter((target) => !target.group);
     result.push(...ungroupedTargets);
 
@@ -176,16 +178,23 @@ export abstract class BaseView {
       targetDefinition: TargetConfiguration
     ]
   ): TargetViewItem {
-    const myConfiguration = configuration;
-    const cofigs = myConfiguration.configurations;
+    // "configuration" is the target config, which can have configurations itself, e.g. "production"
+    const cofigs = configuration.configurations;
     const hasConfigs = cofigs && Object.keys(cofigs).length > 0;
+    const group = configuration.nxConsole?.group ?? '';
     return {
       id: `${nxProject.project}:${targetName}`,
       contextValue: 'target',
       nxProject,
       nxTarget: { name: targetName },
-      group: myConfiguration.group,
-      label: targetName,
+      group: group,
+      /*
+       * Remove the group name from the target label (if present) since it's already displayed in the group.
+       * This allows to name targets like "[format]write" and have them displayed as "write" in the tree.
+       * It avoids target name collions.
+       * E.g. you can have [lint]all and [format]all in the same project.
+       */
+      label: targetName.replace(`[${group}]`, ''),
       collapsible: hasConfigs
         ? TreeItemCollapsibleState.Collapsed
         : TreeItemCollapsibleState.None,
